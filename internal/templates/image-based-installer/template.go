2ee2f7eddcf7e3a15512a336e72b615e726c7e52
Sharat Akhoury <sakhoury@redhat.com>
2025-11-05 11:36:22 -0500

    Add AdditionalNTPSources to default ImageClusterInstall template (#601)
    Signed-off-by: Sharat Akhoury <sakhoury@redhat.com>

--- internal/templates/image-based-installer/template.go
+++ internal/templates/image-based-installer/template.go
@@ -122,6 +122,10 @@ spec:
   proxy:
 {{ .Spec.Proxy | toYaml | indent 4 }}
 {{ end }}
+{{ if .Spec.AdditionalNTPSources }}
+  additionalNTPSources:
+{{ .Spec.AdditionalNTPSources | toYaml | indent 4 }}
+{{ end }}
 `
 
 const BareMetalHost = `apiVersion: metal3.io/v1alpha1
